ff55aeb8aa467ee32e24eca1ccde505cdb3f3b14
Marcus <marcusmiguel0612@gmail.com>
2021-03-09 17:18:43 -0300

    README atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { StickyNavModule } from 'ng2-sticky-nav';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatListModule } from '@angular/material/list';
 import { CardComponent } from './card/card.component';
-
 
 @NgModule({
   declarations: [
@@ -28,17 +23,12 @@ import { CardComponent } from './card/card.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatBadgeModule,
     MatCardModule,
-    MatGridListModule,
     StickyNavModule,
-    MatChipsModule,
     MatFormFieldModule,
-    MatListModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
